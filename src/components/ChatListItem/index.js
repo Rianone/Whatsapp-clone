ae5ba559b53fa4431423164dfb63593bd712225e
Rianone <84690507+Rianone@users.noreply.github.com>
2023-03-28 23:22:17 +0100

    Updated  chat item

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -2,8 +2,16 @@ import { Text, View, Image, StyleSheet } from 'react-native';
 
 function ChatListItem() {
     return ( 
-        <View>
-            <Image source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg" }} style={{ width: 50, height:50} } />
+        <View style={styles.container}>
+            <Image source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg" }} style={styles.image} />
+            
+            <View>
+                <View style={styles.row}>
+                    <Text>Lukas</Text>
+                    <Text>07:30</Text>
+                </View>
+                <Text>Oke</Text>
+            </View>
         </View>
      );
 }
@@ -13,9 +21,18 @@ export default ChatListItem;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        display: "flex",
+        borderWidth: 1,
+        borderColor: '#000',
+        flexDirection: "row",
+        width: "90%",
+        padding: 5
     },
+    row: {
+        display: "flex", 
+        flexDirection: "row",
+        width: "65%",
+        justifyContent: "space-between",
+    },
+    image: { width: 50, height: 50, borderRadius: 50 }
 });
