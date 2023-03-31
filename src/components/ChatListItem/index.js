ecb975381bd372b5feff2f2b36d38bbc4ad10db8
Rianone <84690507+Rianone@users.noreply.github.com>
2023-03-31 12:12:48 +0100

    ChatListItem added

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -5,12 +5,12 @@ function ChatListItem() {
         <View style={styles.container}>
             <Image source={{ uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg" }} style={styles.image} />
             
-            <View>
+            <View style={styles.content}>
                 <View style={styles.row}>
-                    <Text>Lukas</Text>
+                    <Text style={styles.name} numberOfLines={1}>Lukas</Text>
                     <Text>07:30</Text>
                 </View>
-                <Text>Oke</Text>
+                <Text numberOfLines={2} style={styles.subTitle}>Hello world </Text>
             </View>
         </View>
      );
@@ -21,18 +21,32 @@ export default ChatListItem;
 
 const styles = StyleSheet.create({
     container: {
-        display: "flex",
-        borderWidth: 1,
-        borderColor: '#000',
         flexDirection: "row",
-        width: "90%",
-        padding: 5
+        marginHorizontal: 10,
+        marginVertical: 5,
+        height: 70,
+    },
+    content: {
+        flex: 1,
+        borderBottomColor: "lightgrey",
+        borderBottomWidth: StyleSheet.hairlineWidth
+    },
+    name: {
+        flex: 1,
+        fontWeight: "bold"
+    },
+    subTitle: {
+        color: "grey"
     },
     row: {
-        display: "flex", 
-        flexDirection: "row",
-        width: "65%",
-        justifyContent: "space-between",
+        flexDirection: "row", 
+        marginBottom: 5,
     },
-    image: { width: 50, height: 50, borderRadius: 50 }
+    image: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        marginRight: 10
+    }
 });
+
