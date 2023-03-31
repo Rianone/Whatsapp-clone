3fb4c6dacb12569a4a692bebe0ff4d475fed58b0
Rianone <84690507+Rianone@users.noreply.github.com>
2023-03-31 18:13:49 +0100

    Added message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -24,7 +24,16 @@ const styles = StyleSheet.create({
         margin: 5,
         padding: 10,
         borderRadius: 10,
-        maxWidth: "80%"
+        maxWidth: "80%",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
+
+        elevation: 2,
     },
     sent_message: {
         backgroundColor: "white",
